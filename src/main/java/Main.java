5bda3a86d144d452ef0163e976d401ce5b98f0b5
Maksim Safonov <safonov85@mail.ru>
2025-04-28 21:36:20 +0300

    add double print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите тест и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
-        }
+
+        System.out.println("Введите число 1 и нажмите <Enter>:");
+        int firestNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите число 2 и нажмите <Enter>::");
+        int secondNumber = new Scanner(System.in).nextInt();
+        double q = (double) firestNumber / secondNumber;
+        System.out.println(q);
+
+    }
 }
