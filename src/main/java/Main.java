a13f6f4835e7e03e8ec89f2df3526d32fc51ee18
Maksim Safonov <safonov85@mail.ru>
2025-05-01 22:07:10 +0300

    add double print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Введите число 1 и нажмите <Enter>:");
-        int firestNumber = new Scanner(System.in).nextInt();
+        double firestNumber = new Scanner(System.in).nextDouble();
         System.out.println("Введите число 2 и нажмите <Enter>::");
-        int secondNumber = new Scanner(System.in).nextInt();
-        double q = (double) firestNumber / secondNumber;
-        System.out.println(q);
+        double secondNumber = new Scanner(System.in).nextDouble();
+        double s = firestNumber + secondNumber;
+        double n = firestNumber - secondNumber;
+        double y = firestNumber * secondNumber;
+        double q = firestNumber / secondNumber;
+        System.out.println("Сумма: " + s);
+        System.out.println("Разность: " + n);
+        System.out.println("Произведение: " + y);
+        System.out.println("Частное: " + q);
+
 
     }
 }
